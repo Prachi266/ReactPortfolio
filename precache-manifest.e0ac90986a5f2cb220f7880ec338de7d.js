5d4f83ae39d63c4b6ec1f352c60a742de35e6c97
Prachi266 <prachiparihar.p26@gmail.com>
2020-06-30 17:15:54 +0530

    Updates

---
+++ precache-manifest.e0ac90986a5f2cb220f7880ec338de7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601402af0b8ff6f018dd610ab1b63f21",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "56be485fc407a85bd3fc",
+    "url": "/ReactPortfolio/static/css/main.6add785d.chunk.css"
+  },
+  {
+    "revision": "ad7ec229e532e5ca6f01",
+    "url": "/ReactPortfolio/static/js/2.00a87b8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.00a87b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56be485fc407a85bd3fc",
+    "url": "/ReactPortfolio/static/js/main.2179f19a.chunk.js"
+  },
+  {
+    "revision": "97a1c7d0b592075495b7",
+    "url": "/ReactPortfolio/static/js/runtime-main.ad87311a.js"
+  },
+  {
+    "revision": "a6e59f294343d76a16a9f2d0674e004a",
+    "url": "/ReactPortfolio/static/media/WhatsApp Image 2020-05-20 at 1.49.32 AM.a6e59f29.jpeg"
+  },
+  {
+    "revision": "bd5c4e6272b3171ce0befe4ed9f1f1d3",
+    "url": "/ReactPortfolio/static/media/mountains1.bd5c4e62.jpg"
+  }
+]);
